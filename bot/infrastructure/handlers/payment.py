1393f945febf7c82b2d4df3c94c7f2ffff04d843
Temirlan Ilik <0penguin@mail.ru>
2024-04-05 03:49:12 +0500

    update text

--- bot/infrastructure/handlers/payment.py
+++ bot/infrastructure/handlers/payment.py
@@ -14,7 +14,13 @@ async def confirm_ride(call: types.CallbackQuery):
         ride.save()
         await call.bot.send_message(
             chat_id=user.telegram_id,
-            text=f'Ваша поездка {ride.ride.ride_title} к {ride.ride.departure.strftime("%d.%m.%Y %H:%M")} подтверждена.'
+            text=f'Ваша поездка {ride.ride.ride_title} к {ride.ride.departure.strftime("%d.%m.%Y %H:%M")} подтверждена.' \
+                 f'Ваша поездка от KBTU DORM до KBTU успешно подтверждена!' \
+                 f'Пожалуйста, будьте внимательны к времени и не опаздывайте на шаттл, который подъедет к вам с улицы Ислама Каримова. Рекомендуем быть на месте за 5-10 минут до отправления.' \
+                 f'Обратите внимание, что мы не несем ответственности, если вы опоздаете на поездку.' \
+                 f'Пожалуйста, помните, что возврат оплаты возможен при отмене более чем за 12 часов до поездки, но мы также рассматриваем все случаи индивидуально.' \
+                 f'Если у вас возникнут какие-либо вопросы или проблемы, не стесняйтесь обращаться к команде /help.' \
+                 f'Желаем вам приятной поездки и благодарим за доверие к нам!'
         )
         await call.answer('Поездка подтверждена')
     else:
@@ -35,6 +41,3 @@ async def decline_ride(call: types.CallbackQuery):
         await call.answer('Поездка отклонена')
     else:
         await call.answer(f'Поездка уже {ride.status}')
-
-
-# TODO: add ride canceling
