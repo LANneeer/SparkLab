fc2c962657048cfe41698aff6af43759e4bd663b
Temirlan Ilik <0penguin@mail.ru>
2024-04-24 01:53:11 +0500

    new alert

--- bot/management/commands/send_message.py
+++ bot/management/commands/send_message.py
@@ -1,8 +1,10 @@
+import asyncio
+
+from asgiref.sync import sync_to_async
 from django.core.management.base import BaseCommand
+
 from bot.apps import BotConfig
 from users.models import User
-import asyncio
-from asgiref.sync import sync_to_async
 
 
 class Command(BaseCommand):
@@ -10,21 +12,19 @@ class Command(BaseCommand):
         users = await sync_to_async(list)(User.objects.filter(is_staff=False))
         for user in users:
             try:
-                await bot.send_message(chat_id=user.telegram_id, text=text)
+                await bot.send_message(chat_id=user.telegram_id, text=text, disable_web_page_preview=True, parse_mode='HTML')
             except Exception as e:
                 print(f"Failed to send message to {user.telegram_id}: {e}")
 
     def handle(self, *args, **options):
         bot = BotConfig.bot
-        text = "<b>Привет! Благодарим за доверие к нашему сервису.</b>\n" \
-                "16 апреля у нас будет специальное предложение: <u><b>поездка всего за 300 тенге!</b></u>\n\n" \
-                "Выбери удобное время отправления:\n" \
-                "1. 09:10 с Dorm KBTU\n" \
-                "2. 10:10 с Dorm KBTU\n" \
-                "3. 11:10 с Dorm KBTU\n\n" \
-                "Переходи в меню и бронируй поездку на завтра!"
+        text = "<b>Привет, на связи UniShuttle!</b>\n" \
+               "<b>Спасибо за доверие к нашему проекту.<b>\n" \
+               "Мы успешно завершили тестовый период, организовав поездки для 162 пассажиров🤯🥳\n" \
+               "Мы теперь сотрудничаем напрямую с водителями, без участия спонсора. <u>И в связи с этим вынуждены сообщить следующее:</u>\n" \
+               "На основе результатов этих поездок мы пришли к выводу, что для обеспечения стабильной работы нашего проекта <b>необходимо повысить цену до 450 тенге</b>🚌\n" \
+               "💓Нам очень важно ваше мнение, пожалуйста, пройдите очень короткий опрос по ссылке:\n" \
+               "<i><a href='https://forms.gle/QXkh1NwBLufPEZxe7'>Ссылка</a></i>"
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.send_messages(bot, text))
         loop.close()
-
-
